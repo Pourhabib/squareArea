34f338b86357606dce0eadfeca73e6179ce763b0
Pourhabib <siamakpourhabib@SIAMAKs-MacBook-Pro.local>
2016-10-23 01:42:28 -0700

    Why Cant I call the method?

---
+++ SquareArea.playground/Contents.swift
@@ -0,0 +1,35 @@
+//SquareArea
+
+import UIKit
+
+//The Square class has a length property that has a default value of 1. In order to create squares with a different length than 1 we need to write a custom initializer.
+
+
+class Square {
+    var length: Int
+    init(length: Int) {
+        self.length = length
+        
+        func area(length: Int) -> Int {
+            return length * length
+        }
+    }
+}
+
+let firstSquare = Square(length: 8)
+print("The length of the firstsquare is \(firstSquare.length)", terminator: "")
+
+let secondSquare = Square(length: 4)
+print("The length of the secondSquare is \(secondSquare.length)", terminator: "")
+
+
+if firstSquare.length < secondSquare.length {
+    
+    print("The smaller square has a length of \(firstSquare.length) ", terminator: "")
+} else {
+    print("The bigger square has the length of \(firstSquare.length)", terminator: "")
+}
+
+firstSquare.length
+
+
